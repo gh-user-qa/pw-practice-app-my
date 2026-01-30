4d9910de6efe6db4332b6633766f6a2ac0ef0908
QA <qa@QAs-MacBook-Pro.local>
2026-01-30 14:59:06 +0200

    Checkboxes

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -42,3 +42,22 @@ test.describe('Form Layouts page', () => {
 
     })
 })
+
+test('checkboxes', async({page}) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Toastr').click()
+
+    await page.getByRole('checkbox', {name: "Hide on click"}).click({force: true})
+    // await page.getByRole('checkbox', {name: "Hide on click"}).check({force: true})
+    // await page.getByRole('checkbox', {name: "Hide on click"}).uncheck({force: true})
+    await page.getByRole('checkbox', {name: "Prevent arising of duplicate toast"}).check({force: true})
+
+    const allCheckboxes = page.getByRole('checkbox')
+    for (const box of await allCheckboxes.all()) {
+        // await box.check({force: true})
+        // expect(await box.isChecked()).toBeTruthy()
+        await box.uncheck({force: true})
+        expect(await box.isChecked()).toBeFalsy()
+    }
+
+})
