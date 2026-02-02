0451bce5f57d2e44b01b429ec5c2f7e054e5c35e
QA <qa@QAs-MacBook-Pro.local>
2026-02-02 20:27:59 +0200

    lesson-36: Lists and Dropdowns

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -59,5 +59,35 @@ test('checkboxes', async({page}) => {
         await box.uncheck({force: true})
         expect(await box.isChecked()).toBeFalsy()
     }
+})
 
+test('lists and dropdowns', async({page}) => {
+    const dropdownMenu = page.locator('ngx-header nb-select')
+    await dropdownMenu.click()
+
+    page.getByRole('list') //when the list has a UL tag
+    page.getByRole('listitem') // when the list has a LI tag
+
+    // const optionList = page.getByRole('list').locator('nb-option')
+    const optionList = page.locator('nb-option-list nb-option')
+    await expect(optionList).toHaveText(["Light", "Dark", "Cosmic", "Corporate"])
+    await optionList.filter({hasText: "Cosmic"}).click()
+
+    const header = page.locator('nb-layout-header')
+    await expect(header).toHaveCSS('background-color', 'rgb(50, 50, 89)')
+
+    const colors = {
+        "Light": "rgb(255, 255, 255)",
+        "Dark": "rgb(34, 43, 69)",
+        "Cosmic": "rgb(50, 50, 89)",
+        "Corporate": "rgb(255, 255, 255)"
+    }
+    await dropdownMenu.click()
+    for (const color in colors) {
+        await optionList.filter({hasText: color}).click()
+        await expect(header).toHaveCSS('background-color', colors[color])
+        if (color != "Corporate")
+            await dropdownMenu.click()
+
+    }
 })
